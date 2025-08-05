93c90ff64b5917d1413456cfb9b4157bc5b6b41c
Brooklyn-Dev <brooklynbaylisdev@gmail.com>
2025-08-05 23:48:23 +0100

    feat: add all board colours, improve styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import "./App.css";
+import { Colour } from "./types";
+
+// prettier-ignore
+const BOARD_COLOURS = [
+	[Colour.ORANGE, Colour.BLUE, Colour.PURPLE, Colour.PINK, Colour.YELLOW, Colour.RED, Colour.GREEN, Colour.BROWN],
+	[Colour.RED, Colour.ORANGE, Colour.PINK, Colour.GREEN, Colour.BLUE, Colour.YELLOW, Colour.BROWN, Colour.PURPLE],
+	[Colour.GREEN, Colour.PINK, Colour.ORANGE, Colour.RED, Colour.PURPLE, Colour.BROWN, Colour.YELLOW, Colour.BLUE],
+	[Colour.PINK, Colour.PURPLE, Colour.BLUE, Colour.ORANGE, Colour.BROWN, Colour.GREEN, Colour.RED, Colour.YELLOW],
+	[Colour.YELLOW, Colour.RED, Colour.GREEN, Colour.BROWN, Colour.ORANGE, Colour.BLUE, Colour.PURPLE, Colour.PINK],
+	[Colour.BLUE, Colour.YELLOW, Colour.BROWN, Colour.PURPLE, Colour.RED, Colour.ORANGE, Colour.PINK, Colour.GREEN],
+	[Colour.PURPLE, Colour.BROWN, Colour.YELLOW, Colour.BLUE, Colour.GREEN, Colour.YELLOW, Colour.ORANGE, Colour.RED],
+	[Colour.BROWN, Colour.GREEN, Colour.RED, Colour.YELLOW, Colour.PINK, Colour.PURPLE, Colour.BLUE, Colour.ORANGE]
+];
 
 function App() {
+	function getSquareColour(index: number) {
+		const row = Math.floor(index / 8);
+		const col = index % 8;
+		return BOARD_COLOURS[row][col];
+	}
+
 	return (
 		<>
 			<div>
 				<h1>Kamisado</h1>
-				<div className="board">
-					{Array.from({ length: 64 }).map((_, i) => (
-						<div key={i} className="square">
-							{i}
-						</div>
-					))}
+				<div className="board-container">
+					<div className="board">
+						{Array.from({ length: 64 }).map((_, i) => (
+							<div key={i} className="square" style={{ backgroundColor: getSquareColour(i) }}>
+								{i}
+							</div>
+						))}
+					</div>
 				</div>
 			</div>
 		</>
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export const Colour = {
+	ORANGE: "#ff6900",
+	BLUE: "#2b7fff",
+	PURPLE: "#ad46ff",
+	PINK: "#f6339a",
+	YELLOW: "#f0b100",
+	RED: "#fb2c36",
+	GREEN: "#00c950",
+	BROWN: "#973c00",
+} as const;
