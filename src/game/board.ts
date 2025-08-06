58592bfc9fc74a4685382806809cc83f48fded42
Brooklyn-Dev <brooklynbaylisdev@gmail.com>
2025-08-06 22:18:19 +0100

    fix: tower movement directions
    - Made towers move in 8 directions instead of 4

--- src/game/board.ts
+++ src/game/board.ts
@@ -56,6 +56,10 @@ export class Board {
 			{ dr: -1, dc: 0 },
 			{ dr: 0, dc: 1 },
 			{ dr: 0, dc: -1 },
+			{ dr: 1, dc: 1 },
+			{ dr: -1, dc: -1 },
+			{ dr: 1, dc: -1 },
+			{ dr: -1, dc: 1 },
 		];
 
 		for (const { dr, dc } of directions) {
